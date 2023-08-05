5edc09fc0aeb9235a27ecf787e2ab1f4412e0960
Yue Hao <105403119+Yue-Hao14@users.noreply.github.com>
2023-08-05 09:20:18 -0400

    Time: 54 ms (96.68%) | Memory: 14.7 MB (49.37%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # track current index and starts from 1 as the first element is always unique
+        l = 1 
+        # iterate through the list and compare current element vs. the one before
+        for r in range(1,len(nums)):
+            if nums[r] != nums[r-1]:
+                nums[l] = nums[r]
+                l += 1
+            else:
+                r += 1
+
+        return l
