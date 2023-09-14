3364cf2ed45a112746b8f49ba4611db3038f955c
Yue Hao <105403119+Yue-Hao14@users.noreply.github.com>
2023-09-14 12:40:20 -0400

    Time: 108 ms (19.90%) | Memory: 20.5 MB (34.02%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -5,17 +5,25 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        # heat map to store count of each element
+        # bucket sort method, time O(n), space O(n)
+
+        # idea:
+        # count frequency of each num and store in heatmap
+        # put num to a nested frequency array, where index is the frequency of num, inner array holds all num with same frequency
+        # itereate through nested frequency array from back until we get K elements
+
+        # code:
+        # create heat map to store count of each element, key = num, value = count of num in nums
         count = {} 
-        # nested list where index is the count/frequency, inner list holds element with the same frequency
+        # create nested list where index is the count/frequency, inner list holds element with the same frequency
         freq = [[] for i in range(len(nums) + 1)] 
 
-        # count each element and store it in count heat map
+        # count each element and store it in count heatmap
         for n in nums:
             count[n] = 1 + count.get(n,0) 
 
-        # extract element-count pair from hash map
-        # append each element to coresponding count, same count element will be in the same index
+        # extract num-count pair from hash map
+        # append each num to coresponding count, same count element will be in the same index
         for element, count in count.items():
             freq[count].append(element)
         print(freq)
@@ -26,5 +34,6 @@ class Solution(object):
             # add inner list's element to result list until result list's length equal to k, then return result list
             for n in freq[i]:
                 result.append(n)
+                # once len(result) reach k, return result
                 if len(result) == k:
                     return result
