18be69a103e7c4a4f695cd97541107335b5c4e8d
Yue Hao <105403119+Yue-Hao14@users.noreply.github.com>
2023-09-25 14:40:21 -0400

    Time: 53 ms (6.10%) | Memory: 18 MB (34.24%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -6,23 +6,18 @@
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
-        # idea: O(n) space and time
-        # iterate through the list and store value of each node in a stack until get to the last node
-        # once at last node, we start pop the stack to overwrite existing LL
-        pointer = head
-        stack = []
-        
-        # add val to stack
-        while pointer:
-            stack.append(pointer.val)
-            pointer = pointer.next
-        
-        pointer = head # go back to head
-        
-        # overwrite current val by top of stack to reverse
-        while pointer:
-            pointer.val = stack.pop() 
-            pointer = pointer.next
-            
-        return head
+        # idea: O(n) time but O(1) space
+        # iterate through LL and store previous node in a variable, 
+        # assign next to the previous node
+
+        prev = None
+        curr = head
+     
+        while curr:
+            temp_next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp_next
+
+        return prev
         
