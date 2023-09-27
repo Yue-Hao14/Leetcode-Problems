d162ec340cf29a65f43deed4b6bc4c8d6df0e4eb
Yue Hao <105403119+Yue-Hao14@users.noreply.github.com>
2023-09-27 14:55:41 -0400

    Time: 48 ms (18.61%) | Memory: 16.6 MB (81.62%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -6,26 +6,21 @@ class Solution:
         # valid result when open == close == n
         # do a recursion
 
-        array = [] # store each parentheses in an array
         res = [] # store all outputs
 
-        def backtrack(open_n, close_n):
+        def backtrack(open_n, close_n, path):
             # base case
             if open_n == close_n == n:
-                res.append("".join(array))
+                res.append(path)
                 return
 
             # add open parentheses
             if open_n < n:
-                array.append("(")
-                backtrack(open_n + 1, close_n)
-                array.pop() 
+                backtrack(open_n + 1, close_n, path + "(")
 
             # add close parentheses
             if close_n < open_n:
-                array.append(")")
-                backtrack(open_n, close_n + 1)
-                array.pop()
+                backtrack(open_n, close_n + 1, path + ")")
         
-        backtrack(0,0)
+        backtrack(0,0,"")
         return res
