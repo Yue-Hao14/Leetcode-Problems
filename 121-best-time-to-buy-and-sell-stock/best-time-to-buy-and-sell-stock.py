507b15265175e06da4c2a106e7ae17b7d0a9b1d2
Yue Hao <105403119+Yue-Hao14@users.noreply.github.com>
2023-10-10 22:17:25 -0400

    Time: 864 ms (55.79%) | Memory: 27.3 MB (78.48%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,26 +1,22 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        # set 2 pointers
-        # left pointer track when to buy, and wanna stay at lowest price
-        # right pointer track when to sell
-        # iterate through the array to find the maxProfit
-            # if right > left, check if need to update maxProfit, right+1
-            # if right < left, move left to right and right+1
-        left, right, max_profit = 0,1,0
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # idea: time O(n), space O(1)
+        # iterate through prices array
+        # set left and right pointers
+        # if left >= right, move left to right as we find a very low price at right index
+        # if left < right, move right forward, update max_profit
+
+        # code:
+        left, right = 0, 1 # left = buy, right = sell
+        max_profit = 0
 
         while right < len(prices):
-            current_profit = prices[right] - prices[left]
-            # profitable?
-            if current_profit > 0:
-                max_profit = max(current_profit, max_profit)
+            if prices[left] < prices[right]:
+                profit = prices[right] - prices[left]
+                max_profit = max(max_profit, profit)
+                right += 1
             else:
                 left = right
-            right += 1
-
+                right = left + 1
+        
         return max_profit
-
-
